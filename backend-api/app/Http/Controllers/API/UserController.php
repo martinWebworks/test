07da5bc73436a214e663450d04acad62e077fa1e
Martin Pirate <sirmartins6@gmail.com>
2024-01-21 07:43:41 +0300

    added login logic

--- backend-api/app/Http/Controllers/API/UserController.php
+++ backend-api/app/Http/Controllers/API/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Mail\LoginLinkEmail;
+use App\Models\LoginToken;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -14,19 +20,25 @@ class UserController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
-        $request->validate([
-            'email' => 'email|required',
+
+        $request->validate(['email' => 'required|email']);
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = LoginToken::create([
+            'user_id' => $user->id,
+            'token' => Str::random(60),
+            'expires_at' => Carbon::now()->addMinutes(30)
         ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
-            return fractal()->item(Auth::user(), new UserTransformer(false), 'data')
-                ->parseIncludes(['token'])
-                ->serializeWith(new ArraySerializer())
-                ->respond(200, [], JSON_PRETTY_PRINT);
+        $loginUrl = url('/login/' . $token->token);
 
-        } else {
-            return response()->json(['message' => "Invalid email or password"], Response::HTTP_NOT_FOUND);
-        }
+        Mail::to($user->email)->send(new LoginLinkEmail($loginUrl));
+
+        return response()->json(['message' => 'Login link has been sent to your email.']);
     }
+
+
+
 
 }
