07bb11318e30c05aa9d3dccfb1c296a366af72bd
Martin Pirate <sirmartins6@gmail.com>
2024-01-21 09:38:46 +0300

    added template

--- frontend_app/src/main.js
+++ frontend_app/src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createWebHistory, createRouter} from "vue-router";
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import Login from './views/Login.vue'
+import Profile from './views/Profile.vue'
+
+
+const routes = [
+    {
+        path: "/login",
+        component: Login,
+    },
+
+    {
+        path: "/profile",
+        component: Profile,
+    },
+    {path: "/:pathMatch(.*)*", redirect: "/login"},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+createApp(App).use(router).mount('#app')
